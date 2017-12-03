c3467f3fa3a6e344d21ec44699348fec5f31ef59
Agnieszka Koszany <agnieszka.koszany@codecool.com>
2017-12-03 22:14:44 +0100

    Implement solution of the second part

--- day2/corruption_checksum.py
+++ day2/corruption_checksum.py
@@ -3,9 +3,26 @@ def get_checksum(file_name):
 
     f = open(file_name)
     for line in f:
-        numbers = line.split('\t')
+        numbers = line.strip().split('\t')
         convert_list_string_to_int(numbers)
         checksum += max(numbers) - min(numbers)
+    f.close()
+
+    return checksum
+
+def sum_evenly_divisible_values(file_name):
+    checksum = 0
+
+    f = open(file_name)
+    for line in f:
+        numbers = line.strip().split('\t')
+        convert_list_string_to_int(numbers)
+        for i in range(len(numbers)):
+            for j in range(i + 1, len(numbers)):
+                if numbers[i] % numbers[j] == 0:
+                    checksum += numbers[i]/numbers[j]
+                if numbers[j] % numbers[i] == 0:
+                    checksum += numbers[j]/numbers[i]
     f.close()
 
     return checksum
@@ -17,6 +34,7 @@ def convert_list_string_to_int(seq):
 
 def main():
     print(get_checksum("puzzle_input.csv"))
+    print(sum_evenly_divisible_values("puzzle_input.csv"))
 
 if __name__ == '__main__':
     main()
