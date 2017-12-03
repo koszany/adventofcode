68136ad399c702953decdb2705b0969adc4b59af
Agnieszka Koszany <agnieszka.koszany@codecool.com>
2017-12-03 19:58:01 +0100

    Refactor empty lines according to PEP-8

--- day1/inverse_captcha.py
+++ day1/inverse_captcha.py
@@ -1,5 +1,6 @@
 def get_list_of_digits(file):
     seq = []
+
     with open(file) as f:
         for line in f:
             for dig in line:
@@ -9,7 +10,6 @@ def get_list_of_digits(file):
     return seq
 
 def sum_next_digits(seq):
-
     sum_of_digits = 0
 
     for i in range(len(seq)-1):
@@ -22,23 +22,21 @@ def sum_next_digits(seq):
     return sum_of_digits
 
 def sum_halfway_around_digits(seq):
-
     sum_of_digits = 0
     halfway = int(len(seq)/2)
+
     for i in range(halfway):
         if seq[i] == seq[halfway + i]:
             sum_of_digits += seq[i]*2
 
     return sum_of_digits
 
-
 def main():
-
     seq = get_list_of_digits("sequence.csv")
     sum_next = sum_next_digits(seq)
     sum_halfway = sum_halfway_around_digits(seq)
     print(sum_next, sum_halfway)
-
+    
 
 if __name__ == '__main__':
     main()
