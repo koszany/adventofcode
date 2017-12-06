3d83cc0fd74d10514c41b008b9d9f135a91a4b9f
Agnieszka Koszany <agnieszka.koszany@codecool.com>
2017-12-06 21:50:25 +0100

    Implement 1st part - increasing offset by 1/-1

--- day5/maze.py
+++ day5/maze.py
@@ -1,7 +1,27 @@
 def main():
-    print(get_amount_steps('puzzle_input.csv'))
+    print(get_amount_steps_increase('puzzle_input.csv'))
+    print(get_amount_steps_decrease_increase('puzzle_input.csv'))
 
-def get_amount_steps(file_name):
+def get_amount_steps_decrease_increase(file_name):
+
+    maze = get_maze_from_file(file_name)
+    amount_steps = 0
+
+    index = 0
+    while index in range(len(maze)):
+        jump = maze[index]
+
+        if maze[index] < 3:
+            maze[index] +=1
+        else:
+            maze[index] -=1
+
+        index += jump
+        amount_steps += 1
+
+    return amount_steps
+
+def get_amount_steps_increase(file_name):
 
     maze = get_maze_from_file(file_name)
     amount_steps = 0
