d6743aca6bee11360bc2ebd36766a51a003060ce
Agnieszka Koszany <agnieszka.koszany@codecool.com>
2017-12-06 18:11:33 +0100

    Implement searching for amount of no annagram passphrases

--- day4/high-entropy_passphrase.py
+++ day4/high-entropy_passphrase.py
@@ -1,10 +1,19 @@
-def get_amount_valid_passphrases(file_name):
-
+def get_amount_unique_passphrases(file_name):
     passphrases_counter = 0
     with open(file_name) as f:
         for line in f:
             passphrase = line.strip().split(' ')
             if is_valid(passphrase):
+                passphrases_counter += 1
+
+    return passphrases_counter
+
+def get_amount_passphrases_no_annagrams(file_name):
+    passphrases_counter = 0
+    with open(file_name) as f:
+        for line in f:
+            passphrase = line.strip().split(' ')
+            if is_complex_of_no_annagram_words(passphrase):
                 passphrases_counter += 1
 
     return passphrases_counter
@@ -16,8 +25,25 @@ def is_valid(passphrase):
                 return False
     return True
 
+def is_complex_of_no_annagram_words(passphrase):
+    for i in range(len(passphrase)-1, -1, -1):
+        for j in range(i-1, -1, -1 ):
+            if is_annagram(passphrase[i], passphrase[j]):
+                return False
+    return True
+
+def is_annagram(word, pattern):
+    if len(word) == len(pattern):
+        for letter in word:
+            if word.count(letter) != pattern.count(letter):
+                return False
+        return True
+    else:
+        return False
+
 def main():
-    print(get_amount_valid_passphrases('puzzle_input.csv'))
+    print(get_amount_unique_passphrases('puzzle_input.csv'))
+    print(get_amount_passphrases_no_annagrams('puzzle_input.csv'))
 
 if __name__ == '__main__':
     main()
